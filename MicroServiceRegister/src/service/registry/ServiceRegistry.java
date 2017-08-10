792a56ada083ab5ac9aa4db4ffa73e7e03e4850f
prasanna.rajendran <prasanna.rajendran@PRASANNA-WIN7.fc.bil.konicaminolta.us>
2017-08-09 18:07:56 -0700

    2nd Commit

--- MicroServiceRegister/src/service/registry/ServiceRegistry.java
+++ MicroServiceRegister/src/service/registry/ServiceRegistry.java
@@ -3,12 +3,22 @@ package service.registry;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.RetryNTimes;
+import org.jboss.netty.util.internal.ConcurrentHashMap;
+
 public class ServiceRegistry implements IServiceRegistry{
 	
+	private CuratorFramework curatorFramework;
+	private ConcurrentHashMap<String, String> service2APImapping;
 	public ServiceRegistry() {
 		Properties prop=new Properties();
 		try {
 			prop.load(this.getClass().getResourceAsStream("zookeeper.properties"));
+			curatorFramework=CuratorFrameworkFactory.newClient(prop.getProperty("host")+":"+prop.getProperty("port"), new RetryNTimes(5, 1000));
+			curatorFramework.start();
+			service2APImapping=new ConcurrentHashMap<>();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
