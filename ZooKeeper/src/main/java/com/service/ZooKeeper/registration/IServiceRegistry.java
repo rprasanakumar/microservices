d9f541f0404fb91fa4c34a71af985eafac29514d
prasanna.rajendran <prasanna.rajendran@PRASANNA-WIN7.fc.bil.konicaminolta.us>
2017-08-11 18:47:56 -0700

    initial

---
+++ ZooKeeper/src/main/java/com/service/ZooKeeper/registration/IServiceRegistry.java
@@ -0,0 +1,31 @@
+package com.service.ZooKeeper.registration;
+
+public interface IServiceRegistry {
+	
+	
+	/**
+	 * Register a service by path and service name. Creates a znode, type ephemeral 
+	 * 
+	 * For the given path and name, a znode is created. The paths are hierarchical. If the parent node does not exist, it is created.
+	 * 
+	 * 
+	 * @param serviceName- the name of the individual service for identification
+	 * @param path- The endpoint to which the service can handle the request.
+	 * 
+	 */
+	
+	public void registerService(String serviceName, String path)  throws Exception; 
+	
+	
+	
+	
+	/**
+	 * Unregister a service by path. Deletes a znode recursively.
+	 * 
+	 * 
+	 * @param serviceName- the name of the individual service to be deleted
+	 * @param path- The endpoint to which the service was registered.
+	 * 
+	 */
+	public void unregisterService(String serviceName, String path)  throws Exception;
+}
